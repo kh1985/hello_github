0510c2cf20ce56b45923aecc814ab281767263aa
kh <kenjihachiya@kenji.local>
2017-03-18 09:08:12 +0900

    お試し

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello");
+
+        File f = new File("/Users/kenjihachiya/Desktop/work/test/Hello.java");
+
+        if(f.canRead())
+        {
+            System.out.println("読み込みOK");
+        }else
+        {
+            System.out.println("読み込みNG");
+        }
+    }
+}
